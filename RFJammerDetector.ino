c864a7d83446b8783b20b21425c30609e72f15cd
Andrew MacPherson <andrew@andrewmohawk.com>
2019-08-01 09:59:59 +0200

    Added image and wiring

--- RFJammerDetector.ino
+++ RFJammerDetector.ino
@@ -1,6 +1,14 @@
 /* Simple Script to 'detect' jammers, really just looks at the average analog in values   
  * from a cheap 433mhz RX component and then decides if its been held 'low' ( like a remote has been pressed )
  * for too many seconds.
+ * 
+ * Wiring is relatively straight forward:
+ * 
+ * Datapin of 433mhz RX -> analog 'inPin' ( default *analog* 2 )
+ * Green LED -> 'greenLED' ( default 2 )
+ * Red LED -> 'redLED' ( default 3)
+ * Vibration Motor -> 'buzzPin' ( default 11 )
+ * 
 */
 
 int numSamples = 300; // num samples to take every loop
